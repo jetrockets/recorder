fb90fac94a62274233f0c356e241124e35a905d1
igor <igor.alexandrov@jetrockets.ru>
2016-07-08 17:50:54 +0300

    Updated associations parse

--- lib/recorder/tape.rb
+++ lib/recorder/tape.rb
@@ -77,7 +77,7 @@ module Recorder
     end
 
     def parse_associations_attributes(event)
-      if self.item.recorder_options[:associations].any?
+      if self.item.recorder_options[:associations].present?
         self.item.recorder_options[:associations].inject({}) do |hash, association|
           reflection = self.item.class.reflect_on_association(association)
           if reflection.present?
--- lib/recorder/version.rb
+++ lib/recorder/version.rb
@@ -3,7 +3,7 @@ module Recorder
   module VERSION
     MAJOR = 0
     MINOR = 1
-    TINY = 4
+    TINY = 5
     PRE = nil
 
     STRING = [MAJOR, MINOR, TINY, PRE].compact.join(".").freeze
