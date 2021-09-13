1a5a7380c9a314ebba710673e540f85cca4869f6
Julia Egorova <j.e.egorova@gmail.com>
2021-09-13 13:23:34 +0300

    Fix 'uninitialized constant Recorder::Rails::Railtie' error

--- lib/recorder/rails/railtie.rb
+++ lib/recorder/rails/railtie.rb
@@ -2,7 +2,7 @@
 
 module Recorder
   module Rails
-    class Railtie < Rails::Railtie
+    class Railtie < ::Rails::Railtie
       initializer 'recorder.configure' do |_app|
         require 'recorder/sidekiq/revisions_worker' if defined?(Sidekiq)
       end
